9a4b87323d98e5c2a10dac0512a5da0dfcbe9f6d
ACuriousCat <1275517130@qq.com>
2021-04-17 02:43:47 +0800

    Signed-off-by: ACuriousCat <1275517130@qq.com>

--- web/flaskweb.py
+++ web/flaskweb.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>曼曼，二二无人无人v早上好，中午好，晚上好！<h1>'
+    return '<h1>曼曼，二二无人分工表分工不分给你放哪无人v早上好，中午好，晚上好！<h1>'
 
 if __name__ == '__main__':
     app.run(debug=True,port=8888)
